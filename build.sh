41ce1a3863652202a6749b80c3131c412daa24a8
Richard Klafter <richard@zinc.com>
2016-11-21 23:59:53 -0800

    more

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/node_modules/.bin/browserify $DIR/src/index.ts  -p [ tsify --noImplictAny ] --debug > bundle.js
+$DIR/node_modules/.bin/browserify $DIR/lib/index.ts  -p [ tsify --noImplictAny ] --debug > $DIR/src/bundle.js
