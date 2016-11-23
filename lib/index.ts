5481f2ef9d8c9dae3e8f2860d230ad356ccba537
Richard Klafter <richard@zinc.com>
2016-11-22 21:44:14 -0800

    added three.js

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ win['Snap'] = require('snapsvg');
 win['$'] = require('jquery');
 win['_'] = require('underscore');
 win['Backbone'] = require('backbone');
+win['THREE'] = require('three');
 
 import * as j from './fun'
 j.log();
