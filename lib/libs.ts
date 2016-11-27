3e926216be2cf6180a4a0517603d91ff5adfe47c
Richard Klafter <richard@zinc.com>
2016-11-26 22:28:21 -0800

    fix

--- lib/libs.ts
+++ lib/libs.ts
@@ -13,7 +13,9 @@ window['$'] = require('jquery');
 window['_'] = require('underscore');
 window['Backbone'] = require('backbone');
 window['THREE'] = require('three');
+try {
 window['interact'] = require('interact.js');
+} catch(ex) {}
 try {
     window['Snap'] = require('snapsvg');
 } catch(ex) {}
