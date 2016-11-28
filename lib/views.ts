c05bf8ef6a7d1da7137266a0b859191ceace6343
Richard Klafter <richard@zinc.com>
2016-11-27 21:16:29 -0800

    Revert "stop the flashes"
    This reverts commit d5a97612732d4c7178a0d7d49d8baccb742accf5.

--- lib/views.ts
+++ lib/views.ts
@@ -47,13 +47,13 @@ export class HexView extends Backbone.View<Hex> {
         var ele = s.group(poly, money, territory)
 
         this.setElement(ele.node);
-        this.listenTo(this.model, 'change:team', this.render.bind(this, false))
+        this.listenTo(this.model, 'change:team', this.render)
         this.listenTo(this.model, 'change:tenant', this.render)
-        this.listenTo(this.model, 'change:money', this.render.bind(this, false))
-        this.listenTo(this.model, 'change:canMove', this.render.bind(this, false))
+        this.listenTo(this.model, 'change:money', this.render)
+        this.listenTo(this.model, 'change:canMove', this.render)
         this.render();
     }
-    render(ignore?, dontRenderTenant?:boolean):HexView{
+    render():HexView{
         //update classes
         _.range(1, 50).forEach((i)=>this.$el.removeClass('team-'+i))
         Snap(this.el)
@@ -67,10 +67,8 @@ export class HexView extends Backbone.View<Hex> {
         //let territoryEl = Snap(this.el).select('.territory')
         //territoryEl.attr({text: this.model.territory || ''})
         //cleanup old tenant if it exsits
-        if(!dontRenderTenant){
-            $(this.el).find('.sprite').remove();
-        }
-        if(this.model.tenant && !dontRenderTenant){
+        $(this.el).find('.sprite').remove();
+        if(this.model.tenant){
             //get graphics for new tenant
             let svgTable:Dictionary<string> = {}
             svgTable[Tenant.Grave.toString()] = '/img/grave.svg'
