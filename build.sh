c94a7f15c53852928edce59e5ed0e308a90ea80e
Richard Klafter <richard@zinc.com>
2017-01-01 15:44:18 -0800

    faster build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)
-node_modules/.bin/browserify lib/libs.ts --ignore-missing --dg=false  -p [ tsify --noImplictAny ] --debug > src/libs.js
+node_modules/.bin/browserify lib/libs.ts --noParse=three --noParse=jquery --ignore-missing --dg=false  -p [ tsify --noImplictAny ] --debug > src/libs.js
 node_modules/.bin/browserify lib/index.ts --exclude ws --ignore-missing --dg=false  -p [ tsify --noImplictAny ] --debug > src/bundle.js
 node_modules/.bin/browserify lib/aiworker.ts --exclude ws --ignore-missing --dg=false  -p [ tsify --noImplictAny ] --debug > src/aiworker.js
