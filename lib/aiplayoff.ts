3365f3c151e2079920c06913cc60cd6d47c38ceb
Richard Klafter <richard@zinc.com>
2017-01-07 11:18:22 -0800

    aiplayoff better

--- lib/aiplayoff.ts
+++ lib/aiplayoff.ts
@@ -11,24 +11,22 @@ import {main} from './index'
 colors.setTheme({
     team1:'green',
     team2:'blue',
+    team3:'yellow',
+    team4:'purple',
+    team5:'orange',
 })
 
-export interface Config {
-    host:string
-}
-
-export interface AiConfig extends Config {
+export interface AiConfig {
     team:number
     serverAddress:string
+    host:string
+    //after playoff this is populated with result of playoff
+    boardRatio?:number
 }
 
-/*
-export interface ServerConfig  extends Config {
-    size:number
-    seed?:number
-    numberOfTeams?:number
+export interface AiPlayoff {
+    ais:Array<AiConfig>
 }
-*/
 
 export function startAi(config:AiConfig) {
     function log(data:string) {
@@ -46,50 +44,52 @@ export function startAi(config:AiConfig) {
     return child;
 }
 
+
+
 declare var process:any;
-export function aiplayoffMain(ai1Options, ai2Options) {
-    main().then((runtime:SlaughterRuntime)=>{
-        var ai1, ai2;
-        //reg for currentTurn change event
-        runtime.simulator.game.on("change:currentTurn", ()=>{
-            console.log("change:currentTurn")
-            if(runtime.game.currentTeam != 1) return; //only check end conditions on first players turn
-            var winningTeam = runtime.simulator.teamsByRatioOfBoard()[0];
-            var gameRound = (runtime.game.currentTurn-1) / (runtime.game.numberOfTeams+1);
-            if(winningTeam.ratio > .8){
-                ai1.kill()
-                ai2.kill()
-                console.log(`winner ${winningTeam.team} ${winningTeam.ratio}`)
-                process.exit()
-            }
-            else if(gameRound >= 100){
-                ai1.kill()
-                ai2.kill()
-                console.log(`winning ${winningTeam.team} ${winningTeam.ratio}`)
-                process.exit()
-            }
-            else {
-                console.log(`no winner on game round #${gameRound} best candidate is team ${winningTeam.team} with ratio ${(winningTeam.ratio).toFixed(2)}`)
-            }
+export function aiplayoffMain(aiOptions:AiPlayoff):Promise<AiPlayoff> {
+    return main().then((runtime:SlaughterRuntime)=>{
+        var options:Array<AiConfig> = aiOptions.ais.map((aiConfig, index)=>
+            _.extend({}, aiConfig, {team:index+1, host:process.env.host, serverAddress:runtime.network.address})
+        )
+        var ais:Array<any>; //array of ai processes
+        return new Promise((resolve, reject)=>{
+            //reg for currentTurn change event
+            runtime.simulator.game.on("change:currentTurn", ()=>{
+                console.warn("change:currentTurn")
+                if(runtime.game.currentTeam != 1) return; //only check end conditions on first players turn
+                var winningTeam = runtime.simulator.teamsByRatioOfBoard()[0];
+                var gameRound = (runtime.game.currentTurn-1) / (runtime.game.numberOfTeams+1);
+                if(winningTeam.ratio > .8 || gameRound >= 100){
+                    ais.forEach((ai)=>ai.kill()) //stop all the ais
+                    console.warn(`winner is team ${winningTeam.team} on game round #${gameRound} with ratio ${(winningTeam.ratio).toFixed(2)}`)
+                    //update boardRatios
+                    runtime.simulator.teamsByRatioOfBoard().forEach((ratio)=>{
+                        options[ratio.team-1].boardRatio = ratio.ratio;
+                    })
+                    //return results
+                    resolve({ais:options})
+                }
+                else {
+                    console.warn(`no winner on game round #${gameRound} best candidate is team ${winningTeam.team} with ratio ${(winningTeam.ratio).toFixed(2)}`)
+                }
+            })
+            //start the ais!
+            ais = options.map((option)=>startAi(option))
         })
-        //start the ais!
-        ai1 = startAi(_.extend({team:1, host:process.env.host, serverAddress:runtime.network.address}, ai1Options));
-        ai2 = startAi(_.extend({team:2, host:process.env.host, serverAddress:runtime.network.address}, ai2Options));
-    })
+    }) as any;
 }
 
 declare var module:any;
 if (!module.parent) {
-    process.stdin.setEncoding('utf8');
-    var data = "";
-    process.stdin.on('readable', () => {
-        var chunk = process.stdin.read();
-        if (chunk !== null) {
-            data += chunk;
-        }
-    });
-    process.stdin.on('end', () => {
-        data = JSON.parse(data);
-        aiplayoffMain(data['ai1'], data['ai2']);
-    });
+    console.log = console.warn //this script uses stdout for communication... so redirect garbage write to stdin
+    if(process.argv.length <= 2){
+        console.error(`usage: size=16 host=localhost:8080 node ./src/aiplayoff.js '{"ais":[{},{}]}'`)
+        process.exit(1);
+    }
+    aiplayoffMain(JSON.parse(process.argv[2]))
+        .then((res)=>{
+            process.stdout.write(JSON.stringify(res) + '\n');
+            process.exit()
+        })
 }
