0f11759312ac4b6c4f2cff7118deeeb4735cc6b5
Richard Klafter <richard@zinc.com>
2017-02-05 17:13:51 -0800

    docker build tweaks

--- aituning/builddocker.sh
+++ aituning/builddocker.sh
@@ -2,17 +2,18 @@
 set -o errexit
 set -o nounset
 
-#if [ "$#" -eq 0 ] ; then
-#  echo "Usage: step3_buildDockerContainer.sh tiles_dir"
-#  exit 1
-#fi
-
 #get pathing correct
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 cd "$__dir"
 cd ..
 
+#make sure the source is latest build
+echo "building js"
+npm run build
+
 #build & push the image
+echo "building container"
 docker build -t "freethenation/slaughter:latest" .
+echo "pushing container"
 docker push "freethenation/slaughter:latest"
