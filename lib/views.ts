610d2dd0b64550e6074b822570248bb112ce4a80
Richard Klafter <richard@zinc.com>
2017-02-19 09:23:44 -0800

    fix zindex bug

--- lib/views.ts
+++ lib/views.ts
@@ -273,7 +273,7 @@ export class BuildMenu extends Backbone.View<Game> {
                 var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
                 var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
                 // translate the element
-                target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+                target.style.transform = 'translate(' + x + 'px, ' + y + 'px) translateZ(-1px)';
                 // update the posiion attributes
                 target.setAttribute('data-x', x);
                 target.setAttribute('data-y', y);
